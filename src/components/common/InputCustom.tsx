9c33c01c84fa9dc2841d2d9c7859f2c0c79f28ed
SamuelRSADS <samueluba08@gmail.com>
2025-09-11 21:57:28 -0300

    feat(register): implementa formulário de cadastro com validação e variants no InputCustom
    - Adiciona formulário de cadastro com validação usando Zod e react-hook-form
    - Exibe mensagens de erro customizadas para cada campo
    - Integra componentes InputCustom e Button
    - Adiciona exibição/ocultação de senha com ícones
    - Layout responsivo com fundo e estilização
    - Adiciona suporte a variants ('primary' e 'secondary') no InputCustom
    - Utiliza variant 'secondary' nos campos do formulário para melhor personalização visual

--- src/components/common/InputCustom.tsx
+++ src/components/common/InputCustom.tsx
@@ -3,17 +3,23 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: string;
   label: string;
   icon?: React.ReactNode;
+  variant?: 'primary' | 'secondary';
 }
 
+const variants = {
+  primary: 'border-primary-dark',
+  secondary: 'border-primary-light text-secondary-cream ',
+};
+
 const InputCustom = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ error, label, icon, ...props }, ref) => (
+  ({ error, label, icon, variant = 'primary', ...props }, ref) => (
     <div className="w-full m-2">
       <span>{label}</span>
       <div className="relative mt-1">
         <input
           ref={ref}
           {...props}
-          className="border-2 border-primary-dark rounded-md p-2 w-full pr-10"
+          className={`border-2 rounded-md p-2 w-full pr-10 ${variants[variant]}`}
         />
         {icon && (
           <div className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer">
